5948dee03128763e7420037f39c61602ce277240
Jack Walden <j.walden@zengenti.com>
2017-04-05 14:31:43 +0100

    Add Javascript modules template

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,45 @@
  * Description: A modern front-end framework
  * Version: 0.0.1
  */
+
+var app = (function ($) {
+    'use strict';
+
+    // private alias to settings
+    var s;
+
+    return {
+        settings: function() {
+            this.$window        = $(window);
+
+            this.hello			= 'Hello World';  // Example var
+        },
+
+        init: function() {
+            s = new this.settings();
+
+            this.helloWorld();
+
+            this.bindUIActions();       // UI Actions (click, focus etc)
+            this.bindWindowActions();   // Window actions (scrolling, resize etc)
+        },
+
+        helloWorld: function() {  		// Example function
+            console.log(s.hello);  
+        },
+
+        bindUIActions: function() {
+            
+        },
+
+        bindWindowActions: function() {
+            
+        }
+    
+    };
+})(jQuery);
+
+jQuery(document).ready(function() {
+    app.init();
+
+});
